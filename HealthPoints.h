0d93e376f6f80461e08b18c8b5b9d4977dcf6dfc
Dvir Bartov <dvir.bartov1@gmail.com>
2022-05-28 12:35:53 +0300

    Add Healthpoints implementation and queue with partially completed iterator

--- HealthPoints.h
+++ HealthPoints.h
@@ -8,6 +8,7 @@
 
 class HealthPoints {
 public:
+    // TODO: Should we use init list for maxHP? Since we're validating it
     explicit HealthPoints(const int maxHP = 100) :
         m_maxHP(maxHP),
         m_currentHP(maxHP)
@@ -23,20 +24,35 @@ public:
         if(this == &healthPoints) {
             return *this;
         }
+        
         m_maxHP = healthPoints.m_maxHP;
         m_currentHP = healthPoints.m_currentHP;
         return *this;
     }
 
     HealthPoints& operator+=(const int pointsToAdd) {
-        if((m_currentHP + pointsToAdd) > m_maxHP) {
+        if(m_currentHP + pointsToAdd > m_maxHP) {
             m_currentHP = m_maxHP;
         }
 
-        if ((m_currentHP + pointsToAdd < 0)) {
+        if(m_currentHP + pointsToAdd < 0) {
             m_currentHP = 0;
         }
+        
         m_currentHP += pointsToAdd;
+        return *this;
+    }
+    
+    HealthPoints& operator-=(const int pointsToSubtract) {
+        if(m_currentHP - pointsToSubtract < 0) {
+            m_currentHP = 0;
+        }
+        
+        if(m_currentHP - pointsToSubtract > m_maxHP) {
+            m_currentHP = m_maxHP;
+        }
+        
+        m_currentHP -= pointsToSubtract;
         return *this;
     }
 
@@ -53,6 +69,22 @@ private:
     friend bool operator!=(const HealthPoints& healthPoints1, const HealthPoints& healthPoints2) {
         return healthPoints1.m_currentHP != healthPoints2.m_currentHP;
     }
+    
+    friend bool operator>=(const HealthPoints& healthPoints1, const HealthPoints& healthPoints2) {
+        return healthPoints1.m_currentHP >= healthPoints2.m_currentHP;
+    }
+    
+    friend bool operator>(const HealthPoints& healthPoints1, const HealthPoints& healthPoints2) {
+        return healthPoints1.m_currentHP > healthPoints2.m_currentHP;
+    }
+    
+    friend bool operator<=(const HealthPoints& healthPoints1, const HealthPoints& healthPoints2) {
+        return healthPoints1.m_currentHP <= healthPoints2.m_currentHP;
+    }
+    
+    friend bool operator<(const HealthPoints& healthPoints1, const HealthPoints& healthPoints2) {
+        return healthPoints1.m_currentHP < healthPoints2.m_currentHP;
+    }
 
     friend std::ostream& operator<<(std::ostream& output, const HealthPoints& healthPoints) {
         output << healthPoints.m_currentHP << "(" << healthPoints.m_maxHP << ")";
@@ -61,14 +93,20 @@ private:
 };
 
 // TODO: need to deal with symmetry
-HealthPoints operator+(const int pointsToAdd, const HealthPoints& healthPoints1) {
-    HealthPoints result(healthPoints1);
-    return (result += pointsToAdd);
+HealthPoints operator+(const int pointsToAdd, const HealthPoints& healthPoints) {
+    HealthPoints result = healthPoints;
+    return result += pointsToAdd;
 }
 
-HealthPoints operator+(const HealthPoints& healthPoints1, const int pointsToAdd) {
-    HealthPoints result(healthPoints1);
-    return (result += pointsToAdd);
+HealthPoints operator+(const HealthPoints& healthPoints, const int pointsToAdd) {
+    HealthPoints result = healthPoints;
+    return result += pointsToAdd;
+}
+
+// TODO: Should minus operator be symmetric?
+HealthPoints operator-(const HealthPoints& healthPoints, const int pointToSubtract) {
+    HealthPoints result = healthPoints;
+    return result -= pointToSubtract;
 }
 
 
--- Queue.h
+++ Queue.h
@@ -1,48 +1,134 @@
-//
-// Created by 222ro on 21-May-22.
-//
-
 #ifndef MATAM_EX3_QUEUE_H
 #define MATAM_EX3_QUEUE_H
+
+typedef bool (*filterFunction)(int);
+
+// TODO: Make it generic when finished testing
 class Queue {
 public:
-    Queue() :
-    m_front(nullptr),
-    m_back(nullptr),
-    m_size(0)
-    { };
-
+    Queue():
+        m_front(nullptr),
+        m_back(nullptr),
+        m_size(0)
+    {};
+    
+    void pushBack(int& value) {
+        // TODO: Should we really allocate manually?
+        QNode *newNode = new QNode(value);
+        
+        if(m_front == nullptr) {
+            m_front = newNode;
+        }
+        
+        if(m_back != nullptr) {
+            m_back->setNext(newNode);
+        }
+        
+        m_back = newNode;
+        m_size++;
+    }
+    
+    void popFront() {
+        if(m_front == m_back) {
+            m_back = nullptr;
+        }
+        
+        m_front = m_front->getNext();
+        // TODO: Should we use it like that?
+        delete m_front;
+        m_size--;
+    }
+    
     int front() {
         return m_front->getData();
     }
+    
+    class Iterator;
+    
+//    Iterator begin() const {
+//    }
+    
+//    Iterator end() const {
+//        return m_back->getNext();
+//    }
 
     Queue(Queue& queue) = default;
     ~Queue() = default;
+    
+    class InvalidOperation {};
+    
 private:
-
     class QNode {
     public:
-        QNode(int data) :
+        QNode(int& data) :
             m_data(data),
             m_next(nullptr)
-        { };
+        {};
 
-        int getData() const {
+        int& getData() const {
             return m_data;
+        }
+        
+        QNode* getNext() const {
+            return m_next;
         }
 
         void setData(int dataToSet) {
             m_data = dataToSet;
         }
-
+        
+        void setNext(QNode* next) {
+            m_next = next;
+        }
+        
+        // TODO: default copy c'tor and operator=?
+    
     private:
-        int m_data;
-        int* m_next;
+        int& m_data;
+        QNode* m_next;
     };
 
     QNode* m_front;
     QNode* m_back;
     int m_size;
-
 };
+
+class Queue::Iterator {
+public:
+    // T
+    // TODO: Should it be int&?
+    const int& operator*() const {
+        return currentNode->getData();
+    }
+    
+    Iterator& operator++() {
+        currentNode = currentNode->getNext();
+        return *this;
+    }
+    
+    Iterator operator++(int) {
+        Iterator oldIterator = *this;
+        ++*this;
+        return oldIterator;
+    }
+    
+    const bool operator!=(Iterator iterator) {
+        return this->currentNode != iterator.currentNode;
+    }
+    
+    Iterator(const Iterator&) = default;
+    Iterator& operator=(const Iterator&) = default;
+    
+private:
+    const Queue* queue;
+    QNode* currentNode;
+    
+    Iterator(const Queue* queue, QNode* node):
+        queue(queue),
+        currentNode(node)
+    {}
+    
+    friend class Queue;
+};
+
 #endif //MATAM_EX3_QUEUE_H
